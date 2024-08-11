27a10baa99b8a4eaec5a9e43057914ba23f4cbec
GiorgettaR <giorgetta.roberto@gmail.com>
2024-08-11 19:33:44 +0200

    FIX game logic, result message, language continuity

--- js/app.js
+++ js/app.js
@@ -1,76 +1,92 @@
 
-let guessSendEl = document.getElementById('indovina');
-let guessEl = document.getElementById('numero');
+let guessSendEl = document.getElementById('sendGuess');
+let guessEl = document.getElementById('numberGuess');
 resultDiv = document.querySelector('.result');
 
 const array = getPcArray(1, 9, 4);
 console.log('pc ' + array);
 
-guessSendEl.addEventListener('click', function(){
+let guesses = 0;
 
-    let guess = guessEl.value;
-    let guessArray = guess.split('');
-    let guessArrayInt = [];
+guessSendEl.addEventListener('click', function () {
 
-    if (guessArray.length == 4){
+    guesses++;
+    if (guesses <= 10) {
+        let guess = guessEl.value;
+        let guessArray = guess.split('');
+        let guessArrayInt = [];
 
-        for (i = 0; i < 4; i++) {
-            guessArrayInt.push(parseInt(guessArray[i]))
+        if (guessArray.length == 4) {
+
+            for (i = 0; i < 4; i++) {
+                guessArrayInt.push(parseInt(guessArray[i]))
+            }
+
+            let resultString = getResultString(array, guessArrayInt);
+            createResultMessagge(resultString, guess);
+
+        } else {
+            alert('insert 4 numbers between 1 and 9 (no blanks)')
         }
+    } else alert(`You tried guessing the number 10 times, I'm sorry, you lost, refresh the page to play again.`)
 
-        let resultString = getResultString(array, guessArrayInt);
-        createResultMessagge(resultString);
-
-    } else {
-        alert('insert 4 numbers between 1 and 9 (no blanks)')
-    }
 })
 
 
-function getPcArray(min, max, length){
+function getPcArray(min, max, length) {
     let array = [];
-    while (array.length < length){
-        let num = Math.floor(Math.random()*max) + min;
-        if (!array.includes(num)){
+    while (array.length < length) {
+        let num = Math.floor(Math.random() * max) + min;
+        if (!array.includes(num)) {
             array.push(num);
         }
     }
     return array
 }
 
-function getResultString(pc, user){
+function getResultString(pc, userGuess) {
     let resultString = '';
-    for (i = 0; i < 4; i++){
-        // if (pc.includes(user[i])){
-        //     if (pc[i] === user[i]){
-        //         arrayString += 'X';
-        //     } else {
-        //         arrayString += 'O';
-        //     }
-        // }
-        if (pc.indexOf(user[1]) != (-1)) {
-            if (pc[i] === user[i]) {
+    for (i = 0; i < 4; i++) {
+        if (pc.includes(userGuess[i])) {
+            if (pc[i] === userGuess[i]) {
                 resultString += 'X';
             } else {
-                resultString += 'O'
+                resultString += 'O';
             }
-        } 
+        }
+        // if (pc.indexOf(user[i]) != (-1)) {
+        //     if (pc[i] === user[i]) {
+        //         resultString += 'X';
+        //     } else {
+        //         resultString += 'O'
+        //     }
+        // }
     }
     return resultString
 }
 
-function createResultMessagge(resultString) {
+function createResultMessagge(resultString, numberGuess) {
+    // creat result box
     const resultBox = document.createElement('div');
-        resultBox.classList.add('resultBox', 'p-2', 'm-2', 'text-center', 'w-50');
-        if (resultString == '') {
-            resultBox.innerHTML = 'no matches'
-            resultBox.classList.add('border', 'border-danger')
-        } else if (resultString == 'XXXX') {
-            resultBox.innerHTML = 'YOU WIN';
-            resultBox.classList.add('fw-bold', 'border', 'border-success')
-        } else {
-            resultBox.innerHTML = resultString;
-            resultBox.classList.add('border', 'border-info')
-        }
-        resultDiv.append(resultBox);
+    resultBox.classList.add('resultBox', 'p-2', 'text-center', 'w-50', 'd-flex');
+
+    // add user guess
+    const guess = resultBox.appendChild(document.createElement('div'));
+    guess.classList.add('guess', 'p-2', 'm-2');
+    guess.innerHTML = numberGuess;
+
+    // add message relative to user guess
+    const message = resultBox.appendChild(document.createElement('div'));
+    message.classList.add('message', 'flex-grow-1', 'p-2', 'm-2');
+    if (resultString == '') {
+        message.innerHTML = 'NO MATCHES'
+        resultBox.classList.add('border', 'border-danger')
+    } else if (resultString == 'XXXX') {
+        message.innerHTML = 'YOU WIN';
+        resultBox.classList.add('fw-bold', 'border', 'border-success')
+    } else {
+        message.innerHTML = resultString;
+        resultBox.classList.add('border', 'border-info')
+    }
+    resultDiv.append(resultBox);
 }
